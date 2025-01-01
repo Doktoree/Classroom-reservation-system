dfd43418430fa987cfe1ac6e490c4c6f32db813b
Doktoree <tjuring901@gmail.com>
2025-01-01 20:14:33 +0100

    Validation for ReservationDto added

--- backend/src/main/java/doktoree/backend/dtos/ReservationDto.java
+++ backend/src/main/java/doktoree/backend/dtos/ReservationDto.java
@@ -3,6 +3,7 @@ package doktoree.backend.dtos;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import doktoree.backend.domain.Classroom;
@@ -10,6 +11,8 @@ import doktoree.backend.domain.Department;
 import doktoree.backend.domain.StudentOrganization;
 import doktoree.backend.domain.User;
 import doktoree.backend.enums.CouncilType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,13 +25,28 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReservationDto {
 	
+	@NotNull(message = "ID can not be null!")
 	private Long id;
+	
+	@NotNull(message = "Reservation purpose can not be null!")
 	private String reservationPurpose;
+	
+	@NotNull(message = "Date can not be null!")
+	@FutureOrPresent(message = "Date can not be in past!")
     private LocalDate date;
+	
+	@NotNull(message = "Start time can not be null!")
     private LocalTime startTime;
+	
+	@NotNull(message = "End time can not be null!")
     private LocalTime endTime;
+	
+	@NotNull(message = "User can not be null!")
     private User user;
+	
+	@NotNull(message = "Classroom can not be null!")
     private Classroom classroom;
+	
     private String subjectName;
     private CouncilType conucilType;
     private String shortDescription;
